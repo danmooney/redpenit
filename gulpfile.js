1507eabc8b30763eb4d701231c29303ec3c24ca1
Dan Mooney <dan.r.mooney@gmail.com>
2024-08-18 17:28:26 -0500

    Add watch support

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 const through = require('through2');
 const path = require('path');
+const watch = require('gulp-watch');
 gulp.task('scripts', function () {
     return gulp.src('src/**/*.js').pipe(through.obj(function (file, enc, cb) {
         if (file.isBuffer()) {
@@ -13,4 +14,7 @@ gulp.task('scripts', function () {
         cb(null, file);
     })).pipe(concat('main.js')).pipe(gulp.dest('build'));
 });
-gulp.task('default', gulp.series('scripts'));
+gulp.task('watch', function () {
+    watch('src/**/*.js', gulp.series('scripts'));
+});
+gulp.task('default', gulp.series('scripts', 'watch'));
