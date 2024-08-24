83d0721f6e4f6868bf5b901088ccb9acbb0faf04
Dan Mooney <dan.r.mooney@gmail.com>
2024-08-24 08:53:31 -0500

    Use "redpenit-" as a prefix

--- src/download.js
+++ src/download.js
@@ -1,10 +1,30 @@
 import canvas from './lib/elements/canvas.js';
 import downloadButton from "./lib/elements/downloadButton";
+import fileInput from './lib/elements/fileInput.js';
 
 downloadButton.addEventListener('click', function() {
-    const image = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream'); // TODO - png only okay?
+    const image = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream');
+    let fileName = 'redpenit-';
+    let fileExtension = '.png';
+
+    if (fileInput.files.length > 0) {
+        const uploadedFileName = fileInput.files[0].name;
+        const uploadedFileExtension = uploadedFileName.substring(uploadedFileName.lastIndexOf('.'));
+        fileName += uploadedFileName.substring(0, uploadedFileName.lastIndexOf('.'));
+        fileExtension = uploadedFileExtension;
+    } else {
+        fileName += Date.now().toString();
+    }
+
+    // Ensure the filename does not exceed 255 characters
+    if (fileName.length + fileExtension.length > 255) {
+        fileName = fileName.substring(0, 255 - fileExtension.length);
+    }
+
+    fileName += fileExtension;
+
     const link = document.createElement('a');
-    link.download = 'edited-image.png';
+    link.download = fileName;
     link.href = image;
     link.click();
 });
